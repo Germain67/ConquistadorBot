5cfbee73297fa3a0fcfd0f467cf8eb6ca4ceaadc
Unknown <germain_clauss@yahoo.fr>
2018-12-20 21:47:38 +0100

    Add format for better logging

--- components/logger.js
+++ components/logger.js
@@ -4,6 +4,10 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
   ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple(),
+  ),
 });
 
 module.exports = logger;
